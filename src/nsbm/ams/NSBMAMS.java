a151ff7945d143c6c3faa37dd378d85b66307f7f
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-11-09 20:04:39 +0530

    Initial commit

---
+++ src/nsbm/ams/NSBMAMS.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nsbm.ams;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Nimesh
+ */
+public class NSBMAMS extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        
+        Scene scene = new Scene(root,800,600);
+        stage.setTitle("Attendance Management System");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+        
+        Image ico = new Image("resources/img/icon.png");
+        stage.getIcons().add(ico);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
