34f1b7fba76484a380e8bc86b3d3931dbd682b69
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-11-10 09:59:36 +0530

    added signup scene

--- src/nsbm/ams/LoginController.java
+++ src/nsbm/ams/LoginController.java
@@ -5,10 +5,15 @@
  */
 package nsbm.ams;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.Pane;
 
 /**
@@ -19,14 +24,28 @@ import javafx.scene.layout.Pane;
 public class LoginController implements Initializable {
 
     @FXML
-    private Pane paneLogin;
+    private Pane paneMain;
+    @FXML
+    private Button btnAdmin;
+    @FXML
+    private Button btnUser;
+    @FXML
+    private Hyperlink linkSignup;
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void toSignup(ActionEvent event) throws IOException {
+        Pane paneSignup = FXMLLoader.load(getClass().getResource("Signup.fxml"));
+        paneMain.getChildren().setAll(paneSignup);
+    }
     
 }
