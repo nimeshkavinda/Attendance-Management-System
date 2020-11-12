2941faf950282bd660af2aadd4c147fa7a93e303
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-11-12 13:40:05 +0530

    user functions

--- src/nsbm/ams/UserLoginController.java
+++ src/nsbm/ams/UserLoginController.java
@@ -7,11 +7,15 @@ package nsbm.ams;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
@@ -38,6 +42,10 @@ public class UserLoginController implements Initializable {
     private Button btnLogin;
     @FXML
     private Hyperlink linkSignup;
+    
+    Connection con = null;
+    ResultSet rs = null;
+    PreparedStatement ps = null;
 
     /**
      * Initializes the controller class.
@@ -58,10 +66,67 @@ public class UserLoginController implements Initializable {
 
     @FXML
     private void loginUser(ActionEvent event) {
+        
+        String email = txtEmail.getText();
+        String pass = txtPass.getText();
+        
+        if(event.getSource() == btnLogin){
+            
+            if(email.isEmpty() || pass.isEmpty()){
+                
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Missing");
+                alert.setHeaderText("Looks like you have missed something");
+                alert.setContentText("Please fill in all the fields");
+                
+                alert.showAndWait();
+                
+            }
+            else if(!email.matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")){
+            
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Invalid data");
+                alert.setHeaderText(null);
+                alert.setContentText("Email should be valid");
+
+                alert.showAndWait();
+            
+            }
+            else{
+                
+                con = DatabaseConnection.ConnectDatabase();
+                
+                String qry = "select * from employee where email = ? and password = ?";
+                
+                try
+                {
+                    ps = con.prepareStatement(qry);
+                    ps.setString(1, email);
+                    ps.setString(2, pass);
+                    rs = ps.executeQuery();
+                    
+                    if (rs.next())
+                    {
+                        System.out.println("user found");
+                    }
+                    else
+                    {
+                        System.out.println("no user");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        
     }
 
     @FXML
-    private void toSignup(ActionEvent event) {
+    private void toSignup(ActionEvent event) throws IOException {
+        Pane paneSignup = FXMLLoader.load(getClass().getResource("Signup.fxml"));
+        paneUserLogin.getChildren().setAll(paneSignup);
     }
     
 }
