033ab2975827bcae913bb65fe58822e0f717f468
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-11-18 18:43:47 +0530

    added gmail smtp class

---
+++ src/nsbm/ams/SendEmail.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nsbm.ams;
+
+import java.util.Properties;    
+import javax.mail.*;    
+import javax.mail.internet.*; 
+
+/**
+ *
+ * @author Nimesh
+ */
+public class SendEmail {
+    
+    String recipientEmail;
+
+    public SendEmail(String stdemail) {
+        
+        final String username = "nimeshkavinda13@gmail.com";
+        final String password = "";
+        
+        recipientEmail = stdemail;
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        
+        Session session;
+        session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(recipientEmail)
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+}
