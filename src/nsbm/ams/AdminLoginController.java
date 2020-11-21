cc02f696b61c7d358f2151c34ddad0170d60b767
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-11-21 12:52:40 +0530

    adminlogincontroller

--- src/nsbm/ams/AdminLoginController.java
+++ src/nsbm/ams/AdminLoginController.java
@@ -7,6 +7,10 @@ package nsbm.ams;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
@@ -42,6 +47,13 @@ public class AdminLoginController implements Initializable {
     private Button btnLogin;
     @FXML
     private Hyperlink linkSignup;
+    
+    String fname;
+    String lname;
+    String flname;
+    Connection con = null;
+    ResultSet rs = null;
+    PreparedStatement ps = null;
 
     /**
      * Initializes the controller class.
@@ -62,13 +74,80 @@ public class AdminLoginController implements Initializable {
 
     @FXML
     private void loginAdmin(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
-        Stage dashboard = (Stage)((Node) event.getSource()).getScene().getWindow();
-        dashboard.setScene(new Scene(root,1200, 700));
-        dashboard.show();
-        dashboard.centerOnScreen();
-        dashboard.setTitle("Attendance Management System");
-        dashboard.setResizable(false);
+
+        String email = txtEmail.getText();
+        String pass = txtPass.getText();
+        
+        if(event.getSource() == btnLogin){
+            
+            if(email.isEmpty() || pass.isEmpty()){
+                
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Missing");
+                alert.setHeaderText("Looks like you have missed something");
+                alert.setContentText("Please fill in all the fields");
+                
+                alert.showAndWait();
+                
+            }
+            else if(!email.matches("([a-zA-Z0-9\\.]{1,20})\\@nsbm[\\.]lk")){
+            
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Invalid data");
+                alert.setHeaderText(null);
+                alert.setContentText("Please use NSBM provided email to login to dashboard");
+
+                alert.showAndWait();
+            
+            }
+            else{
+                
+                con = DatabaseConnection.ConnectDatabase();
+                
+                String qry = "select * from employee where email = ? and password = ? and access_lvl = '0' ";
+                
+                try{
+                    ps = con.prepareStatement(qry);
+                    ps.setString(1, email);
+                    ps.setString(2, pass);
+                    rs = ps.executeQuery();
+                    
+                    if (rs.next()){
+                        
+                        fname = rs.getString("fname");
+                        lname = rs.getString("lname");
+                        flname = fname + " " + lname;
+                        
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
+                        Parent root = loader.load();
+                        Stage dashboard = (Stage)((Node) event.getSource()).getScene().getWindow();
+                        dashboard.setScene(new Scene(root,1200, 700));
+                        dashboard.show();
+                        dashboard.centerOnScreen();
+                        dashboard.setTitle("Attendance Management System");
+                        dashboard.setResizable(false);  
+                        
+                        DashboardController dashctrl = loader.getController();
+                        dashctrl.setName(flname);
+                        
+                    }
+                    else{
+                            
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Invalid data");
+                        alert.setHeaderText("There's no matching user");
+                        alert.setContentText("Make sure you are using NSBM provided email and your email and password combination is correct");
+
+                        alert.showAndWait();
+                    }
+                }
+                catch (SQLException ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+        }    
+        
     }
 
     @FXML
--- src/nsbm/ams/DashboardController.java
+++ src/nsbm/ams/DashboardController.java
@@ -5,9 +5,22 @@
  */
 package nsbm.ams;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Calendar;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +29,67 @@ import javafx.fxml.Initializable;
  */
 public class DashboardController implements Initializable {
 
+    @FXML
+    private Pane paneNav;
+    @FXML
+    private Hyperlink linkSignout;
+    @FXML
+    private Hyperlink linkManageacc;
+    @FXML
+    private Pane paneMenu;
+    @FXML
+    private Pane paneUpdates;
+    @FXML
+    private ImageView picProfile;
+    @FXML
+    private Label lblGreeting;
+    @FXML
+     Label lblName;
+    
+    Calendar cal;
+    int timeOfDay;
+
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        cal = Calendar.getInstance();
+        timeOfDay = cal.get(Calendar.HOUR_OF_DAY);
+
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            lblGreeting.setText("Good Morning");        
+        }
+        else if(timeOfDay >= 12 && timeOfDay < 16){
+            lblGreeting.setText("Good Afternoon"); 
+        }
+        else if(timeOfDay >= 16 && timeOfDay < 21){
+            lblGreeting.setText("Good Evening"); 
+        }
+        else if(timeOfDay >= 21 && timeOfDay < 24){
+            lblGreeting.setText("Good Night"); 
+        }
+        else{
+            lblGreeting.setText("Hello There");
+        }
+    }
+
+    public void setName(String name){
+       lblName.setText(name);
+    }
+
+    @FXML
+    private void toLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root,800, 600));
+        stage.show();
+        stage.centerOnScreen();
+        stage.setTitle("Attendance Management System");
+        stage.setResizable(false);
+    }
     
 }
