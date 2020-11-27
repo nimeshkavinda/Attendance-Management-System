96bb33e3d7f3610c67adbc33fe460e2a27c345e7
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-11-27 11:47:52 +0530

    Resource allocate scene

--- src/nsbm/ams/AllocateResourceController.java
+++ src/nsbm/ams/AllocateResourceController.java
@@ -5,10 +5,12 @@
  */
 package nsbm.ams;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.Pane;
@@ -34,7 +36,9 @@ public class AllocateResourceController implements Initializable {
     }    
 
     @FXML
-    private void toHome(ActionEvent event) {
+    private void toHome(ActionEvent event) throws IOException {
+        Pane paneMenu = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        paneResourceAllocate.getChildren().setAll(paneMenu);
     }
     
 }
--- src/nsbm/ams/StudentReportController.java
+++ src/nsbm/ams/StudentReportController.java
@@ -15,8 +15,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -28,9 +26,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-import javafx.scene.control.SortEvent;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -39,7 +35,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
-import javafx.util.Callback;
 
 /**
  * FXML Controller class
